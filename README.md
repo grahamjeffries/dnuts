b06d3e75f12496edf735c6ba4a8e8ad1f2b1a3b7
Graham Jeffries <graham.r.jeffries@gmail.com>
2016-05-12 13:28:19 -0400

    initial commit with basic map sketch

---
+++ README.md
@@ -0,0 +1,4 @@
+## Maine Doughnut Map
+This is a simple Leaflet-based map of renouned doughnut shops in Maine, presented as part of the *Hole History Show: Origins of the American-style Donut*.
+
+The map style can be controlled in Mapbox Studio.
---
+++ dmap.js
@@ -0,0 +1,29 @@
+
+var map = L.map('dmap', {
+    center: [45.213, -69.30176],
+    zoom: 7
+});
+
+L.tileLayer('https://api.mapbox.com/styles/v1/gjeffries/cio4k0fg20059akkwif4ul9kb/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ2plZmZyaWVzIiwiYSI6ImNpbzFnY2Q5ODFhaTd1Z2x5Nzh5cmV0YWUifQ.oOXh4iccp59ElAtUZVjeAw').addTo(map);
+
+function onEachFeature(feature, layer) {
+    var popupContent = "<div class=name>" + feature.properties.name + "</div>" +
+        "<div class=address>" + feature.properties.street + "<br>" + feature.properties.town + "</div>" +
+        "<div class=desc>" + feature.properties.desc + "</div>";
+
+    layer.bindPopup(popupContent);
+}
+
+L.geoJson(dnutShops, {
+    onEachFeature: onEachFeature,
+
+    pointToLayer: function (feature, latlng) {
+        return L.marker(latlng, {icon: L.icon({
+        			iconUrl: feature.properties.icon,
+        			iconSize: [30, 30],
+        			iconAnchor: [16, 37],
+        			popupAnchor: [0, -28]
+        		})
+            });
+    }
+}).addTo(map);
