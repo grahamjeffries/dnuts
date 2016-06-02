657abbfeea9539e6f29de38232ac1dff1b28be32
Graham Jeffries <graham.r.jeffries@gmail.com>
2016-06-02 13:54:18 -0400

    updated slippy map with additional assets

--- dmap.js
+++ dmap.js
@@ -1,10 +1,13 @@
 
 var map = L.map('dmap', {
     center: [45.213, -69.30176],
-    zoom: 7
+    zoom: 7,
+    attributionControl: false
 });
 
-L.tileLayer('https://api.mapbox.com/styles/v1/gjeffries/cio4k0fg20059akkwif4ul9kb/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ2plZmZyaWVzIiwiYSI6ImNpbzFnY2Q5ODFhaTd1Z2x5Nzh5cmV0YWUifQ.oOXh4iccp59ElAtUZVjeAw').addTo(map);
+var mb_url = 'https://api.mapbox.com/styles/v1/gjeffries/cioyfpaf70007b3ngg0e7v9a1/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ2plZmZyaWVzIiwiYSI6ImNpbzFnY2Q5ODFhaTd1Z2x5Nzh5cmV0YWUifQ.oOXh4iccp59ElAtUZVjeAw'
+
+L.tileLayer(mb_url).addTo(map);
 
 function onEachFeature(feature, layer) {
     var popupContent = "<div class=name>" + feature.properties.name + "</div>" +
@@ -27,11 +30,11 @@ L.geoJson(dnutShops, {
 
     pointToLayer: function (feature, latlng) {
         return L.marker(latlng, {icon: L.icon({
-        			iconUrl: feature.properties.icon,
-        			iconSize: [30, 30],
-        			iconAnchor: [16, 37],
-        			popupAnchor: [0, -28]
-        		})
+                    iconUrl: feature.properties.icon,
+                    iconSize: [30, 30],
+                    iconAnchor: [16, 37],
+                    popupAnchor: [0, -28]
+                })
             });
     }
 }).addTo(map);
