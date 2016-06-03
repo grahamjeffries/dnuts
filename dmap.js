70bd8612dc93511f9678eed337640fc8017b777c
Graham Jeffries <graham.r.jeffries@gmail.com>
2016-06-03 01:20:28 -0400

    clean up js, remove popup close tick

--- dmap.js
+++ dmap.js
@@ -1,8 +1,8 @@
 
 var map = L.map('dmap', {
-    center: [45.213, -69.30176],
-    zoom: 7,
-    attributionControl: false
+  center: [45.213, -69.30176],
+  zoom: 7,
+  attributionControl: false
 });
 
 var mb_url = 'https://api.mapbox.com/styles/v1/gjeffries/cioyfpaf70007b3ngg0e7v9a1/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZ2plZmZyaWVzIiwiYSI6ImNpbzFnY2Q5ODFhaTd1Z2x5Nzh5cmV0YWUifQ.oOXh4iccp59ElAtUZVjeAw'
@@ -10,31 +10,35 @@ var mb_url = 'https://api.mapbox.com/styles/v1/gjeffries/cioyfpaf70007b3ngg0e7v9
 L.tileLayer(mb_url).addTo(map);
 
 function onEachFeature(feature, layer) {
-    var popupContent = "<div class=name>" + feature.properties.name + "</div>" +
-        "<div class=address>" + feature.properties.street + "<br>" + feature.properties.town + "</div>" +
-        "<div class=desc>" + feature.properties.desc + "</div>";
+  var popupContent = "<div class=icon><img src=" + feature.properties.icon + ' align="middle"></div>' +
+    "<div class=name>" + feature.properties.name + "</div>" +
+    "<div class=address>" + feature.properties.street + "<br>" + feature.properties.town + "</div>" +
+    "<div class=desc>" + feature.properties.desc + "</div>";
 
-    layer.bindPopup(popupContent);
+  var popupOptions = {
+    'closeButton': false
+  };
 
-    layer.on('mouseover', function (e) {
-        this.openPopup();
-    });
-    layer.on('mouseout', function (e) {
-        this.closePopup();
-    });
+  layer.bindPopup(popupContent, popupOptions);
 
-}
+  layer.on('mouseover', function (e) {
+    this.openPopup();
+  });
+  layer.on('mouseout', function (e) {
+    this.closePopup();
+  });
+};
 
 L.geoJson(dnutShops, {
-    onEachFeature: onEachFeature,
+  onEachFeature: onEachFeature,
 
     pointToLayer: function (feature, latlng) {
-        return L.marker(latlng, {icon: L.icon({
-                    iconUrl: feature.properties.icon,
-                    iconSize: [50, 50],
-                    iconAnchor: [16, 37],
-                    popupAnchor: [0, -28]
-                })
-            });
+      return L.marker(latlng, {icon: L.icon({
+        iconUrl: feature.properties.icon,
+        iconSize: [50, 50],
+        iconAnchor: [16, 37],
+        popupAnchor: [0, -28]
+      })
+    });
     }
 }).addTo(map);
